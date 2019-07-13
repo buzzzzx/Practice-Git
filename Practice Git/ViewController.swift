b3023f55a4e2030591e9e557adddc65b0e91909c
binja <buzzzzx233@gmail.com>
2019-07-13 16:06:57 +0800

    Initial Commit

---
+++ Practice Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice Git
+//
+//  Created by jediii on 2019/7/13.
+//  Copyright © 2019 Jediii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
