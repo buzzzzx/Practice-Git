426aa113ab60be0315b0fb6f736a5f322681fa77
binja <buzzzzx233@gmail.com>
2019-07-13 16:18:35 +0800

    Create cat design button

--- Practice Git/ViewController.swift
+++ Practice Git/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func doCatDesign() {
+        // Do cat design
+    }
 
 }
 
